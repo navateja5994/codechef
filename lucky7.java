700c29a22408ef69b7488e42550fda8073dc4f2e
navateja5994 <navatejar80@gmail.com>
2024-12-06 11:53:28 +0530

    Add files via upload

---
+++ lucky7.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+ Scanner sc = new Scanner(System.in);
+        
+        String S = sc.nextLine();
+        
+        System.out.println(S.charAt(6));
+        
+        sc.close();
+    }
+}
