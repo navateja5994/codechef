eba0c7bf31773e299d660c85afee9f322f0a75e9
navateja5994 <navatejar80@gmail.com>
2024-12-06 11:25:21 +0530

    Add files via upload

---
+++ mirror.java
@@ -0,0 +1,14 @@
+import java.util.Scanner; 
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        
+        System.out.println(N);
+        
+        sc.close();
+    }
+}
