1dfde7bf6b3e8e5a47ec0ef4e176e83996564004
NAVATEJA <navatejar80@gmail.com>
2024-12-07 10:00:02 +0530

    Add files via upload

---
+++ codechefmake.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+        if(day == 4) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+	}
+}
