e36ed66e940a8a32d6865feb02f4b84d59c29157
NAVATEJA <navatejar80@gmail.com>
2024-12-09 09:40:59 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int totalLeaves = 3 * (N - 1) + 4;
+        System.out.println(totalLeaves);
+
+	}
+}
