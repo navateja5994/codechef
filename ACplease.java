f7465f3e04ea2d58338dd54d19a89c8e709ebed9
NAVATEJA <navatejar80@gmail.com>
2024-12-09 11:47:24 +0530

    Add files via upload

---
+++ ACplease.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+        int temperature = sc.nextInt();
+        
+        if (temperature > 30) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+	}
+}
