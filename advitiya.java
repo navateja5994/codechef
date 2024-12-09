2aa1a6378baa94e84ca116c2be2bbbabf8d2adbe
NAVATEJA <navatejar80@gmail.com>
2024-12-09 11:45:14 +0530

    Add files via upload

---
+++ advitiya.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        if (N >= 16 && N <= 18) {
+            System.out.println("ADVITIYA");
+        } else {
+            System.out.println("WAITING FOR ADVITIYA");
+        }
+
+	}
+}
