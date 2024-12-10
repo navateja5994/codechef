5246a35e581d878688eb734f8f7168fb52030842
NAVATEJA <navatejar80@gmail.com>
2024-12-10 13:44:27 +0530

    Add files via upload

---
+++ goldcoin.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+
+        if (X > Y) {
+            System.out.println(A);
+        } else {
+            System.out.println(B);
+        }
+
+	}
+}
