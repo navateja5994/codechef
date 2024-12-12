e436548ecc65e8ccfd8e32b79d76e1c1f1df4c2f
NAVATEJA <navatejar80@gmail.com>
2024-12-12 09:49:19 +0530

    Add files via upload

---
+++ sports.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        
+        if (X >= 8 && X <= 10) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+        
+        scanner.close();
+
+	}
+}
