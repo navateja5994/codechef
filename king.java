6c42472c06a6739d02fa984fa647bdfa999753e4
NAVATEJA <navatejar80@gmail.com>
2024-12-12 14:53:21 +0530

    Add files via upload

---
+++ king.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+        for (int i = 0; i < T; i++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            
+            int maxPeople = (N * 5) + (M * 7);
+            System.out.println(maxPeople);
+        }
+
+	}
+}
