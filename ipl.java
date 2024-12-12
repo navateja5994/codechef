437d076476e92ca0ca92d0be88b14164f1d302b9
NAVATEJA <navatejar80@gmail.com>
2024-12-12 14:07:34 +0530

    Add files via upload

---
+++ ipl.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+        for (int i = 0; i < T; i++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            System.out.println(Math.max(0, N - M));
+        }
+
+	}
+}
