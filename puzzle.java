dc2e9ece0c45b8d452ad4643910250c445627e07
NAVATEJA <navatejar80@gmail.com>
2024-12-12 14:16:31 +0530

    Add files via upload

---
+++ puzzle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        if (N >= 6 && N <= 8) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+	}
+}
