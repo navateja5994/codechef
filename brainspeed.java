e30a28a0b16da78f4fc59be050a5845faa5e096b
NAVATEJA <navatejar80@gmail.com>
2024-12-13 12:36:37 +0530

    Add files via upload

---
+++ brainspeed.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+        if (Y > X) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+		
+	}
+}
